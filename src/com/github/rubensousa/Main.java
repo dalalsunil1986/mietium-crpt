584b2f61fa8370b546802b762d116de1138a94c2
ruben <rubensousa.mieti@gmail.com>
2015-09-18 16:23:48 +0100

    Initial commit

---
+++ src/com/github/rubensousa/Main.java
@@ -0,0 +1,27 @@
+package com.github.rubensousa;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
